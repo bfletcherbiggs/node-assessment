e97b71208fd4b99d531b19b93d8a2ff69cf30b72
Benjamin Biggs <benjaminbiggs@MacBook-Pro.local>
2017-03-10 18:58:12 -0600

    one error

--- server.js
+++ server.js
@@ -8,8 +8,52 @@ var userCtrl = require('./userCtrl.js');
 
 app.use(bodyParser.json())
 
+app.get('/api/users', function(req,res){
+
+  if(req.query.favorites){
+    return res.status(200).json(userCtrl.getUsersByFavorite(req.query.favorites))
+  }
+  else if (req.query.age){
+    return res.status(200).json(userCtrl.getUsersByAgeLimit(req.query.age))
+  }
+  else if (req.query.lastname){
+    return res.status(200).json(userCtrl.findUserByQuery('last_name',req.query.lastname))
+  }
+  else if (req.query.email){
+    return res.status(200).json(userCtrl.findUserByQuery('email',req.query.email))
+  }
+  res.status(200).json(userCtrl.readAll())
+})
+app.get('/api/users/:id', function(req,res){
+  let userById = userCtrl.findUserById(parseInt(req.params.id))
+  if(!userById){
+    return res.status(404).json("User not found")
+  }
+  else res.status(200).json(userById)
+})
+app.get('/api/admins', function(req,res){
+  res.status(200).json(userCtrl.getAdmins())
+})
+app.get('/api/nonadmins', function(req,res){
+  res.status(200).json(userCtrl.getNonAdmins())
+})
+app.put('/api/users/:id', function(req,res){
+  let userId = parseInt(req.params.id)
+  let userObj = req.body
+
+  res.status(200).json(userCtrl.updateUser(userId,userObj))
+})
+app.post('/api/users', function(req,res){
+  res.status(200).json(userCtrl.createUser(req.body))
+})
+app.delete('/api/users/:userId', function(req,res){
+  let userId = parseInt(req.params.userId)
+  res.status(200).json(userCtrl.removeUser(userId))
+})
+
+//
 app.listen(port, function(){
   console.log(`Listening on ${port}`)
 })
 
-module.exports = app;
+// module.exports = app;
--- userCtrl.js
+++ userCtrl.js
@@ -1,16 +1,48 @@
 var users = require('./users.js');
-
+const userList = users.find()
 module.exports={
   readAll : function(){
-    users.index()
+    return users.find()
   },
-  findUserById:function(userId){},
-  getAdmins:function(){},
-  getNonAdmins:function(){},
-  getUsersByFavorite:function(favorite){},
-  getUsersByAgeLimit:function(age){},
-  findUserByQuery:function(param,value){},
-  createUser:function(user){},
-  updateUser:function(userId,obj){},
-  removeUser:function(userId){}
+  findUserById:function(userId){
+    return users.findOne('id',userId)
+  },
+  getAdmins:function(){
+    return users.find('type','admin')
+  },
+  getNonAdmins:function(){
+    return users.find('type', 'user')
+  },
+  getUsersByFavorite:function(favorite){
+    let favUsers =[]
+    for(var user of userList){
+      for(var f=0; f<user.favorites.length; f++){
+        if(user.favorites[f] === favorite){
+          favUsers.push(user)
+        }
+      }
+    }
+    return favUsers
+  },
+  getUsersByAgeLimit:function(age){
+    if (userList.filter(ele=> ele.age < age) === []){
+      return null
+    }
+    return userList.filter(ele=> ele.age < age)
+  },
+  findUserByQuery:function(param,value){
+    if(param === "email"){
+      return users.findOne('email',value)
+    }
+    return users.find(param.toLowerCase(),value)
+  },
+  createUser:function(user){
+    return users.add(user)
+  },
+  updateUser:function(userId,obj){
+    return users.update('id',userId,obj)
+  },
+  removeUser:function(userId){
+    return users.remove('id',userId)
+  }
 }
